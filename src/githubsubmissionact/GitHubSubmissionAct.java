2466684802a6b82093b97a6aa05296b2f55b9c16
ST10477757 <ST10477757@vcconnect.edu.za>
2025-10-30 08:48:05 +0200

    Version one

---
+++ src/githubsubmissionact/GitHubSubmissionAct.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubsubmissionact;
+
+/**
+ *
+ * @author skubjana
+ */
+public class GitHubSubmissionAct {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("This activity is just intended to align you with the submission requirements for PROG6112 assessments!!!!!");
+        
+        System.out.println("""
+                           Submission Instructions
+                           -----------------------
+                           1. Navigate to the root folder for the activity to be submitted.
+                           2. Open CMD, terminal or Powershell from the root folder.
+                           3. Use the provided GitHub commands document(Pushing code through to GitHub)
+                           4. Make sure to verify the submission on GitHub, then paste the submission link on Arc, then submit.
+                           5. Then you should be good to go!!!
+                           """);
+    }
+    
+}
